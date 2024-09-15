d6873b255189876e44c08f79d37023d7db1ed06f
jatnerubia <jasonlerit077@gmail.com>
2024-09-15 12:08:59 +0800

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import "dotenv/config"
 import express, { type Application } from "express"
+import cors from "cors"
 import routes from "@/routes"
 
 const app: Application = express()
 
+app.use(cors())
 app.use(express.json({ limit: "5mb" }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 
